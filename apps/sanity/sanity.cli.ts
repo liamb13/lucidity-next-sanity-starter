8e235fd0a58af1f9dcc6948244956ede69ed7b4c
Jamie Warburton <jamie@hexdigital.com>
2024-10-21 08:44:45 +0100

    feat: set up Sanity to use path alias @

--- apps/sanity/sanity.cli.ts
+++ apps/sanity/sanity.cli.ts
@@ -1,13 +1,21 @@
-import {defineCliConfig} from 'sanity/cli'
+import path from 'path';
+import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
     projectId: '882lz72r',
     dataset: 'lucidity-example'
   },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
-  autoUpdates: true,
-})
+
+  vite: config => {
+    if (!config.resolve) config.resolve = {};
+    if (!config.resolve.alias) config.resolve.alias = {};
+
+    config.resolve.alias = {
+      ...config.resolve.alias ?? {},
+      '@': path.resolve(__dirname),
+    }
+
+    return config;
+  },
+});
