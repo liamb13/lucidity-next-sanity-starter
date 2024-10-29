da6a7b9a9972baeb84d534eedf1ed405c4217aed
Jamie Warburton <jamie@hexdigital.com>
2024-10-29 18:04:21 +0000

    feat: add initial tests with vitest

--- packages/modular-content-blocks/constants/index.js
+++
@@ -1,5 +0,0 @@
-export var BLOCK_FIELDSETS;
-(function (BLOCK_FIELDSETS) {
-  BLOCK_FIELDSETS['META'] = 'meta';
-  BLOCK_FIELDSETS['MEDIA'] = 'media';
-})(BLOCK_FIELDSETS || (BLOCK_FIELDSETS = {}));
--- packages/sanity-toolkit/studio/constants/roles.js
+++
@@ -1,8 +0,0 @@
-/**
- * The names of roles in Sanity. These can be customised on the Enterprise plan.
- */
-export var ROLE;
-(function (ROLE) {
-  ROLE['ADMINISTRATOR'] = 'administrator';
-  ROLE['DEVELOPER'] = 'developer';
-})(ROLE || (ROLE = {}));
