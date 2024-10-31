f491a5b229fe0d7886217879b1f2200fd7607081
Jamie Warburton <jamie@hexdigital.com>
2024-10-31 00:36:32 +0000

    feat: remove json pane as not needed

--- apps/sanity/structure/defaultViews.ts
+++ apps/sanity/structure/defaultViews.ts
@@ -6,17 +6,12 @@ import {
 } from 'sanity/structure';
 import { LuPencilLine } from 'react-icons/lu';
 import { isDeveloperOrAdmin } from '@pkg/sanity-toolkit/studio/utilities/roles';
-import { JsonPane } from '@pkg/sanity-toolkit/studio/panes/json-pane';
 
 export function defaultViews(
   S: StructureBuilder,
   context: DefaultDocumentNodeContext,
 ): Array<View | ViewBuilder> {
   const views: Array<View | ViewBuilder> = [S.view.form().icon(LuPencilLine)];
-
-  if (isDeveloperOrAdmin(context.currentUser)) {
-    views.push(JsonPane(S));
-  }
 
   // if (PREVIEWABLE_DOCUMENT_TYPES.includes(context.schemaType)) {
   //   views.push(previewPane(S, context, options.apiVersion));
