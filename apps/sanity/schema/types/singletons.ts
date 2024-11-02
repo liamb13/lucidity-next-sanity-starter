ba3c41375462414a4e18b53756d846837153e5af
Jamie Warburton <jamie@hexdigital.com>
2024-11-02 17:04:45 +0000

    feat(active-theme): add active theme and bring in announcements

--- apps/sanity/schema/types/singletons.ts
+++ apps/sanity/schema/types/singletons.ts
@@ -3,8 +3,10 @@ import { recyclingBinDocument } from '@pkg/sanity-toolkit/recycling-bin/schema';
 import { SINGLETON } from '@pkg/common/constants/schemaTypes';
 import { appConfig } from '@/config/app';
 import { configSeo } from '@/schema/types/singletons/configSeo';
+import { theme } from '@/schema/types/singletons/theme';
 
 export const singletons: SchemaTypeDefinition[] = [
   configSeo,
+  theme,
   recyclingBinDocument(SINGLETON.RECYCLING_BIN, { apiVersion: appConfig.apiVersion }),
 ];
