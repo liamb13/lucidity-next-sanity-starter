fe7f3378dd90197405751970af5794fa0db6b946
Jamie Warburton <jamie@hexdigital.com>
2024-11-04 15:55:11 +0000

    docs(action-resolver): add inline documentation link for document actions

--- apps/sanity/actions/index.ts
+++ apps/sanity/actions/index.ts
@@ -5,6 +5,8 @@ import {
 import { DocumentActionsResolver } from 'sanity';
 import { changePublishText } from '@pkg/sanity-toolkit/studio/actions/changePublishText';
 
+// See: https://www.sanity.io/docs/document-actions
+
 const useLabelPublishOrUpdate = defineActionModifier(() => ({
   actions: ['publish'],
   handler: (action) => changePublishText(action, 'Update'), // Use "Update" if document already published
