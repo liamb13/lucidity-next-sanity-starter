5f2d82a37262ea449c5826a6a424d0d0022d7322
Jamie Warburton <jamie@hexdigital.com>
2025-01-16 22:18:12 +0000

    feat: set up type generation

--- packages/common/constants/schemaTypes.ts
+++ packages/common/constants/schemaTypes.ts
@@ -1,7 +1,7 @@
 /**
  * List of all the schema types we use in Sanity, as constants.
- * This allows us to refer to them by their constant symbol, rather than a magic string, in both
- * the Sanity Studio app, and the web app.
+ * This allows us to refer to them by their constant symbol, rather than a magic string,
+ * in both the Sanity Studio app, and the web app.
  *
  * See: @/docs/repo-architecture/about-constants.md
  *
@@ -10,43 +10,82 @@
  *     CHANGING THE VALUES WILL CAUSE CONTENT TO DISAPPEAR FROM THE CMS        //
  * !!!!!!                         !!!!!!!!                              !!!!!! //
  *
- * This is because these constants are the Sanity `_type` values. If they change, you must also migrate
- * your content to change its `_type` to the new value. If you have no content yet, change them all you like.
+ * This is because these constants are the Sanity `_type` values. If they change,
+ * you must also migrate your content to change its `_type` to the new value.
+ * If you have no content yet, change them all you like.
  */
 
-export enum DOCUMENT {
-  PAGE = 'page',
+/**
+ * 16/01/2025 - Jamie
+ *
+ * Due to Sanity Typegen not supporting enums or objects in queries, we've had to also
+ * export a single variable version of each document type.
+ *
+ * Use the single variable versions within queries, to allow them to be typegen'd:
+ *   e.g. defineQuery(`*[_type == "${DOCUMENT_PAGE}"]`)
+ *
+ * When Sanity Typegen supports objects and enums, we can remove all the single variables
+ * and change the codebase back to just using the enum, e.g. just `${DOCUMENT.PAGE}`.
+ * The issue can be tracked here: https://github.com/sanity-io/sanity/issues/8293
+ * The commit where this was changed is here (to help change back): TBC
+ */
 
-  ARTICLE = 'article',
+export const DOCUMENT_PAGE = 'page';
+export const DOCUMENT_ARTICLE = 'article';
+export const DOCUMENT_AUTHOR = 'author';
+export const DOCUMENT_TAXONOMY_CATEGORY = 'category';
+export const DOCUMENT_TAXONOMY_TAG = 'tag';
+export const DOCUMENT_CONFIG_REDIRECT = 'config.redirect';
+export const DOCUMENT_CONFIG_CORE_SECTION = 'config.coreSection';
+export const DOCUMENT_CONFIG_REUSABLE_BLOCK = 'config.reusableBlock';
+export const DOCUMENT_ANNOUNCEMENT = 'announcement';
+export const DOCUMENT_NAVIGATION_HEADER = 'navigation.header';
+export const DOCUMENT_NAVIGATION_FOOTER = 'navigation.footer';
+export const DOCUMENT_MEDIA_TAG = 'media.tag'; // Added by the Sanity Media Plugin
 
-  AUTHOR = 'author',
+export const enum DOCUMENT {
+  PAGE = DOCUMENT_PAGE,
 
-  TAXONOMY_CATEGORY = 'category',
-  TAXONOMY_TAG = 'tag',
+  ARTICLE = DOCUMENT_ARTICLE,
 
-  CONFIG_REDIRECT = 'config.redirect',
-  CONFIG_CORE_SECTION = 'config.coreSection',
-  CONFIG_REUSABLE_BLOCK = 'config.reusableBlock',
+  AUTHOR = DOCUMENT_AUTHOR,
 
-  ANNOUNCEMENT = 'announcement',
-  NAVIGATION_HEADER = 'navigation.header',
-  NAVIGATION_FOOTER = 'navigation.footer',
+  TAXONOMY_CATEGORY = DOCUMENT_TAXONOMY_CATEGORY,
+  TAXONOMY_TAG = DOCUMENT_TAXONOMY_TAG,
 
-  MEDIA_TAG = 'media.tag', // Added by the Sanity Media Plugin
+  CONFIG_REDIRECT = DOCUMENT_CONFIG_REDIRECT,
+  CONFIG_CORE_SECTION = DOCUMENT_CONFIG_CORE_SECTION,
+  CONFIG_REUSABLE_BLOCK = DOCUMENT_CONFIG_REUSABLE_BLOCK,
+
+  ANNOUNCEMENT = DOCUMENT_ANNOUNCEMENT,
+  NAVIGATION_HEADER = DOCUMENT_NAVIGATION_HEADER,
+  NAVIGATION_FOOTER = DOCUMENT_NAVIGATION_FOOTER,
+
+  MEDIA_TAG = DOCUMENT_MEDIA_TAG, // Added by the Sanity Media Plugin
 }
 
-export enum OBJECT {
-  MODULAR_OUTER_BLOCKS = 'modularContentBlocks.outer',
-  MODULAR_INNER_BLOCKS = 'modularContentBlocks.inner',
+export const OBJECT_MODULAR_OUTER_BLOCKS = 'modularContentBlocks.outer';
+export const OBJECT_MODULAR_INNER_BLOCKS = 'modularContentBlocks.inner';
+export const OBJECT_MODULAR_OUTER_BLOCK_SECTION = 'modularContentBlocks.outer.section';
+export const OBJECT_MODULAR_INNER_BLOCK_RAW_HTML = 'modularContentBlocks.inner.rawHtml';
 
-  MODULAR_OUTER_BLOCK_SECTION = 'modularContentBlocks.outer.section',
+export const enum OBJECT {
+  MODULAR_OUTER_BLOCKS = OBJECT_MODULAR_OUTER_BLOCKS,
+  MODULAR_INNER_BLOCKS = OBJECT_MODULAR_INNER_BLOCKS,
 
-  MODULAR_INNER_BLOCK_RAW_HTML = 'modularContentBlocks.inner.rawHtml',
+  MODULAR_OUTER_BLOCK_SECTION = OBJECT_MODULAR_OUTER_BLOCK_SECTION,
+
+  MODULAR_INNER_BLOCK_RAW_HTML = OBJECT_MODULAR_INNER_BLOCK_RAW_HTML,
 }
 
-export enum SINGLETON {
-  THEME = 'theme',
-  CONFIG_SEO = 'config.seo',
-  CONFIG_404 = 'config.404',
-  RECYCLING_BIN = 'recycling.bin',
+export const SINGLETON_THEME = 'theme';
+export const SINGLETON_CONFIG_SEO = 'config.seo';
+export const SINGLETON_CONFIG_404 = 'config.404';
+export const SINGLETON_RECYCLING_BIN = 'recycling.bin';
+
+export const enum SINGLETON {
+  THEME = SINGLETON_THEME,
+  CONFIG_SEO = SINGLETON_CONFIG_SEO,
+  CONFIG_404 = SINGLETON_CONFIG_404,
+  RECYCLING_BIN = SINGLETON_RECYCLING_BIN,
 }
